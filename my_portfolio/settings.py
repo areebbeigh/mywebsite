6272f34bfd61c177ffe16522599ca61b37f577ae
Areeb Beigh <areebbeigh@gmail.com>
2016-09-20 13:04:37 +0530

    Update

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$%#Tdfert34#$%@#$#fwefr23wr#(QDW3rwefefew#!$WEF'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['areeb12.pythonanywhere.com']
 
 
 # Application definition
